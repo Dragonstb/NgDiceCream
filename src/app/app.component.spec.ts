adfbd3e08174c6203524f1b8195cd3476c87a2c2
Dragonstb <Dragonstb@users.noreply.github.com>
2025-07-22 17:47:49 +0200

    Added karma browser plugins and config for testing
    Also updated the pregenerated app.component.spec.ts to the as-is situation.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -22,8 +22,9 @@ describe('AppComponent', () => {
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
+    const app = fixture.componentInstance;
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, NgDiceCream');
+    expect(compiled.querySelector('h1')?.textContent).toContain(app.title);
   });
 });
